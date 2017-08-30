f186ace61e7f9455812c29f7a63ea80821300ad7
huriyeseberoglu <huriye_seberoglu1907@hotmail.com>
2017-08-30 17:12:59 +0300

    form gönder

--- resources/views/Backend/users/create.blade.php
+++ resources/views/Backend/users/create.blade.php
@@ -8,19 +8,29 @@
 @section('content')
     <form role="form" action="{{route('users.store')}}" method="POST">
 
-        <div class="form-group">
+        <div class="form-group" {{ $errors->has('name') ? ' has-error' : '' }}>
             <label>Name</label>
             <input name="name" class="form-control" placeholder="User's name">
+            @if($errors-> has('name'))
+                <p class="help-block">{{$errors ->first('name')}}</p>
+            @endif
+
         </div>
 
-        <div class="form-group">
+        <div class="form-group" {{ $errors->has('email') ? ' has-error' : '' }}>
             <label>E- Mail</label>
             <input name="email" class="form-control" placeholder="User's  email">
+            @if($errors-> has('email'))
+                <p class="help-block">{{$errors ->first('email')}}</p>
+            @endif
         </div>
 
-        <div class="form-group">
+        <div class="form-group" {{ $errors->has('password') ? ' has-error' : '' }}>
             <label>Password</label>
             <input name="password" type="password" class="form-control" placeholder="User's  password">
+            @if($errors-> has('password'))
+                <p class="help-block">{{$errors ->first('password')}}</p>
+            @endif
         </div>
 
         <button type="submit" class="btn btn-default">Submit Button</button>
