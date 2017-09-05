62072e1d3d8514579477c0d4311443d6b376c175
huriyeseberoglu <huriye_seberoglu1907@hotmail.com>
2017-09-05 10:36:45 +0300

    ek

---
+++ app/Contracts/UserContract.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: BanuPC
+ * Date: 5.09.2017
+ * Time: 10:25
+ */
+
+interface  UserContract
+{
+    
+}
---
+++ app/Http/Repositories/Eloquent/UserRepository.php
@@ -0,0 +1,13 @@
+<?php
+ namespace  App\Http\Repositories\Eloquent;
+ /**
+ * Created by PhpStorm.
+ * User: BanuPC
+ * Date: 5.09.2017
+ * Time: 10:31
+ */
+
+Class UserRepository
+{
+
+}
