58c55617f5a103e6cb605a9be016eacbc9a997ae
huriyeseberoglu <huriye_seberoglu1907@hotmail.com>
2017-09-07 10:10:43 +0300

    deneme2

--- resources/views/Backend/common/buttons.blade.php
+++ resources/views/Backend/common/buttons.blade.php
@@ -1,3 +1,3 @@
-<a href="" class="btn btn-xs btn-info ">Show</a>
+<a href="" class="btn btn-xs btn-info ">Deneme</a>
 <a href="" class="btn btn-xs btn-warning ">Edit</a>
 <a href="" class="btn btn-xs btn-danger ">Delete</a>
